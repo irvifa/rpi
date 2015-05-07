65350ee93231961486d006c13b923979953ba2f5
root <root@raspberrypi.(none)>
2015-05-07 00:31:20 +0000

    edit blacklist script

--- tools/blacklist/blacklist-module.sh
+++ tools/blacklist/blacklist-module.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp ../../conf/blacklist.conf /etc/modprobe.d/
-cat blacklist.conf >> alsa-base-blacklist.conf
+cp ../../conf/alsa-base-blacklist.conf /etc/modprobe.d/
+cp ../../conf/evdev-blacklist.conf /etc/modprobe.d/
 depmod -ae
 update-initramfs -u
 reboot
