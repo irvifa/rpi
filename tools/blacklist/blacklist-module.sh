c6b29de39627155e6c6da11c74569b13511baf4d
Irvi Firqotul Aini <irvi.fa@gmail.com>
2015-11-23 20:20:22 +0700

    retsructure

--- tools/blacklist/blacklist-module.sh
+++ tools/blacklist/blacklist-module.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-cp ../../conf/alsa-base-blacklist.conf /etc/modprobe.d/
-cp ../../conf/evdev-blacklist.conf /etc/modprobe.d/
+cp ../../conf/blacklist/alsa-base-blacklist.conf /etc/modprobe.d/
+cp ../../conf/blacklist/evdev-blacklist.conf /etc/modprobe.d/
+cp ../../conf/blacklist/joydev-blacklist.conf /etc/modprobe.d/
 depmod -ae
 update-initramfs -u
 reboot
