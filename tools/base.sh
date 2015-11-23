99c1df234de6a6aacaee8b22736949c05680e08b
Irvi Firqotul Aini <irvi.fa@gmail.com>
2015-11-23 21:58:17 +0700

    added dhcp server

--- tools/base.sh
+++ tools/base.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # fresh install rpi wheezy
 
+dpkg-reconfigure ntp
 cp ../conf/sources.list /etc/apt/sources.list
 apt-get upgrade && apt-get update
 apt-get install -y nmap curl elinks isc-dhcp-server hostapd
