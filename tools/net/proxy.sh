17a3347492142bf24b767dbc1525120434f88444
Irvi Firqotul Aini <irvi.fa@gmail.com>
2015-12-10 20:05:47 +0700

    edit

--- tools/net/proxy.sh
+++ tools/net/proxy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export http_proxy="152.168.24.10:8080"
-export https_proxy="152.168.24.10:8080"
+export http_proxy=http://152.118.24.10:8080
+export https_proxy=http://152.168.24.10:8080
