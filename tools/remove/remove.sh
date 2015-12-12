9dd78c30011bf3931aec024268fb2c6909bef171
Irvi Firqotul Aini <irvi.fa@gmail.com>
2015-12-13 01:05:06 +0700

    remove uneeded package

--- tools/remove/remove.sh
+++ tools/remove/remove.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo apt-get remove ghostscript idle idle-python2.7 x11-common python3
+sudo apt-get remove ghostscript idle idle-python2.7 x11-common python3 alsa* aptitude ghostscript libmenu-cache1 aptitude-common ed esound-common gconf* gnuplot* gsfonts gsettings-desktop-schemas gstreamer* imagemagick* libsoundtouch0 libswscale2 libts-0.0-0 libv4l* libval libxfc* lxd* menu-* miecraft-pi netpbm poppler* python3*  raspberrypi-artwork tcl 
+sudo apt-get autoremove
