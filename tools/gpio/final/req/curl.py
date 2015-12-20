fd1187c99904d3c29406d8a5a5d8bd3a6ac80a3d
Irvi Firqotul Aini <irvi.fa@gmail.com>
2015-12-20 20:26:36 +0700

    added curl.py example

--- tools/gpio/final/req/curl.py
+++ tools/gpio/final/req/curl.py
@@ -2,11 +2,11 @@ import urllib2
 import json
 
 for i in range(0,10):
-	val = 20
+	val = i
 	data = {"Level":val}
 	url = 'https://chocolate-furry.herokuapp.com/level'
 	req = urllib2.Request(url, json.dumps(data), {'Content-Type': 'application/json'})
 	f = urllib2.urlopen(req)
 	for x in f:
-    	print(x)
+    		print(x)
 	f.close()
